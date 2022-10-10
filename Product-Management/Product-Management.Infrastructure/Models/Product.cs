14bf0179b99d2198b7ce9e44f603d95203697055
ruboarm <ruboarm@gmail.com>
2022-10-10 18:27:56 +0400

    Infrastructure

---
+++ Product-Management/Product-Management.Infrastructure/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management.Infrastructure.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [ConcurrencyCheck]
+        public bool Available { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? DateCreated { get; set; }
+    }
+}
