3b339eba8de724f2f7cd79523d1128c032ed45ba
ruboarm <ruboarm@gmail.com>
2022-10-11 11:39:37 +0400

    Add Services

---
+++ Product-Management-API/Product-Management-API.Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product_Management_API.Infrastructure;
+using Product_Management_API.Infrastructure.Models;
+
+namespace Product_Management_API.Services
+{
+    public class ProductService
+    {
+        public readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            List<Product> list = await _context.Products.ToListAsync();
+            return list;
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            Product? product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            return product;
+        }
+
+
+        public async Task CreateProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteProductAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductByIdAsync(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if(product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
