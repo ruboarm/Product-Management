1ff3a18c7912804b2a38586b9f73ff823b4335a0
ruboarm <ruboarm@gmail.com>
2022-10-12 22:36:17 +0400

    Update Infrastructure with working DbContext

---
+++ Product-Management-API/Product-Management-API.Infrastructure/Models/RegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management_API.Infrastructure.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords don't match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
